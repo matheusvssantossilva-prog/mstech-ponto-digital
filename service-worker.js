ec94a8ca36fd246c3df0c5fd77195e0e8db4a157
Mstech <matheusvssantossilva@gmail.com>
2026-03-02 11:29:32 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,17 @@
-self.addEventListener("install", function(event) {
+self.addEventListener("install", event => {
   console.log("App instalado");
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.open("ponto-cache").then(cache => {
+      return cache.match(event.request).then(response => {
+        return response || fetch(event.request).then(fetchRes => {
+          cache.put(event.request, fetchRes.clone());
+          return fetchRes;
+        });
+      });
+    })
+  );
 });
